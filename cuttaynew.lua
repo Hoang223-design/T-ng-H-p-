bad6243cc799130ddeb85048111d5b03a3ce9438
Hoang223-design <hoanglovesin@gmail.com>
2025-02-27 23:09:33 +0700

    Add files via upload

---
+++ cuttaynew.lua
@@ -0,0 +1,50 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Hoàng.RK Hub",
+         Animation = "create: Ngọc Hoàng 210👑"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=11625890044",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+    local Tab1o = MakeTab({Name = "Script Full CN"})
+    local Tab2o = MakeTab({Name = "Script Auto Chest"})
+    local Tab3o = MakeTab({Name = "Script Auto Nhặt Trái"})
+    local Tab1o = MakeTab({Name = "Script Aimbot:)"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  })
